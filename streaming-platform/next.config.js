e805b6b0165ef5e810cd95f7746872dd94a236e7
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-09-08 21:33:57 +0200

    Configure

---
+++ streaming-platform/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Supprimez la ligne suivante si elle existe
+  // experimental: { appDir: true },
+}
+
+module.exports = nextConfig
